240241822be16d3b22aa590c23931cc0ff24b718
Jacob Gadikian <faddat@gmail.com>
2016-03-05 23:21:50 +0700

    For now this is a linux-only affair
    but will add mac soon.

---
+++ fuckthem.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#PIDGIN
+hg clone https://hg.pidgin.im/pidgin/main pidgin-main
+#WHATSAPP
+git clone https://github.com/davidgfnet/whatsapp-purple
+#SKYPE
+git clone https://github.com/EionRobb/skype4pidgin
+#HANGOUTS
+hg clone https://bitbucket.org/EionRobb/purple-hangouts
+#LWQQ
+git clone https://github.com/xiehuc/pidgin-lwqq
+#WINE FOR QQ
+git clone git://source.winehq.org/git/wine.git
+
+cd pidgin-main
