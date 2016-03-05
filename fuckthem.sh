8da21579783a6320751e76a4b026c4b3804ee8e3
Jacob Gadikian <faddat@gmail.com>
2016-03-06 01:40:23 +0700

    Update fuckthem.sh
    Running list of dependencies:
    
    PIDGIN:
    - intltool
    - meanwhile
    - gplugin  
       - you will need to htmoo
    
    - cmake

--- fuckthem.sh
+++ fuckthem.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+mkdir ~/git
+cd ~/git
+
 #PIDGIN
 hg clone https://hg.pidgin.im/pidgin/main pidgin-main
 #WHATSAPP
@@ -12,4 +15,18 @@ git clone https://github.com/xiehuc/pidgin-lwqq
 #WINE FOR QQ
 git clone git://source.winehq.org/git/wine.git
 
+#DEPENDENCIES
+#gplugin
+hg clone https://bitbucket.org/gplugin/main/
+#moonscript
+
+#luarocks
+
+
+#GPLUGIN DEPENDENCIES
+git clone https://github.com/GNOME/gobject-introspection  #GOBJECT-INTROSPECTION
+cd ~/git/gobject-introspection
+
+
 cd pidgin-main
+
